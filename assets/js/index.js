d8a7e4762fbc68b0bc1aa60b82fc6371a03b79be
tomthestrom <thruska29@gmail.com>
2020-10-04 21:54:58 +0200

    small refactor, BINDABLE_PAIRS now an IIFE, added comments

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,32 +1,37 @@
 document.addEventListener('DOMContentLoaded', () => {
     const BINDABLE_ATTR = 'data-bind';
-    const BINDED = document.querySelectorAll(`[${BINDABLE_ATTR}]`);
+    const BINDABLES     = document.querySelectorAll(`[${BINDABLE_ATTR}]`);
 
-    const modifiablePairs = {};
+    /**
+     * create our pairs, stored under the key of BIND_TO
+     * value in format:
+     * {'currentValue': *value*
+     * 'inputFrom': '<input> element'
+     * 'outputTo': '!'<input>' smth like h1/h2/smth else'}
+     */
+    const BINDABLE_PAIRS = (function (bindables) {
+        this.bindablePairs = {};
 
-    /*  
-    create key value pairs for binding => {}
+        bindables.forEach(bindable => {
+            const NEW_PAIR = {};
+            const BIND_TO = bindable.getAttribute(`${BINDABLE_ATTR}`);
+            
+            if (!this.bindablePairs.hasOwnProperty(BIND_TO)) {
+                this.bindablePairs[BIND_TO] = NEW_PAIR;
+            }
+            //check if it's input type text || its eg an h1/h2/smth else that is getting the value
+            if (bindable.type === "text") {
+                this.bindablePairs[BIND_TO]['currentValue'] = bindable.value;
+                this.bindablePairs[BIND_TO]['inputFrom'] = bindable;
+            } else {
+                this.bindablePairs[BIND_TO]['outputTo'] = bindable;
+            }
+        });
+
+        return this.bindablePairs;
+    })(BINDABLES);
     
-    */
-    BINDED.forEach(element => {
-        const PAIR = {};
-        const BIND_TO = element.getAttribute(`${BINDABLE_ATTR}`);
-        
-        if (!modifiablePairs.hasOwnProperty(BIND_TO)) {
-            modifiablePairs[BIND_TO] = PAIR;
-        }
 
-        if (element.type === "text") {
-            modifiablePairs[BIND_TO]['currentValue'] = element.value;
-            modifiablePairs[BIND_TO]['inputFrom'] = element;
-        } else {
-            modifiablePairs[BIND_TO]['outputTo'] = element;
-        }
 
-         
-        
-    })
-    console.log(modifiablePairs);
-
-    
+    console.log(BINDABLE_PAIRS);
 });
