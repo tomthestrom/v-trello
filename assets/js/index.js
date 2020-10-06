b6bd4407692812ebcc61a13310f399376de7f65a
tomthestrom <thruska29@gmail.com>
2020-10-06 13:29:08 +0200

    H1 modifying input growing mechanism - index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -31,9 +31,52 @@ document.addEventListener('DOMContentLoaded', () => {
         return this.bindablePairs;
     })(BINDABLES, BINDABLE_ATTR);
 
+
+    function isValidValue (newValue) {
+        return newValue.length > 0 && (/\S/.test(newValue));
+    };
+
     for (let bindable of Object.keys(BINDABLE_PAIRS)) {
         BINDABLE_PAIRS[bindable].inputFrom.addEventListener('focusout', e => {
-            BINDABLE_PAIRS[bindable].outputTo.innerText = e.target.value;
+            if (isValidValue(e.target.value)) {
+                BINDABLE_PAIRS[bindable].outputTo.innerText = e.target.value;
+            } else {
+                e.target.value = BINDABLE_PAIRS[bindable].currentValue; 
+            }
         });
     }
+
+
+    const FOCUS_CLICK_ATTR = 'data-click-focus-target';
+    const FOCUS_DESTINATION_ATTR = 'data-click-focus-destination';
+    const FOCUS_CLICK_GROUP = document.querySelectorAll(`[${FOCUS_CLICK_ATTR}]`);
+    
+    FOCUS_CLICK_GROUP.forEach(focusTriggeringElement => {
+        focusTriggeringElement.addEventListener('click', e => {
+            const FOCUS_DESTINATION_ATTR_VALUE = e.target.dataset.clickFocusTarget;
+            const FOCUS_TARGET_ELEMENT         = document.querySelector(`[${FOCUS_DESTINATION_ATTR}=${FOCUS_DESTINATION_ATTR_VALUE}]`);
+                FOCUS_TARGET_ELEMENT.focus();
+                FOCUS_TARGET_ELEMENT.select();
+            }
+        )
+    });
+
+    //grow input
+
+    //we need to resize input as the user is typing, to keep it aesthetical, we need to think about the size of each char :/
+
+    const GROW_INPUT_ELEMENT    =  document.querySelector('[data-grow-input]');
+    const INPUT_REFERENCE       = document.querySelector('[data-grow-input-reference]');
+
+    const INPUT_REFERENCE_CLONE = INPUT_REFERENCE.cloneNode(true);
+
+    
+    INPUT_REFERENCE.insertAdjacentElement('afterend', INPUT_REFERENCE_CLONE);
+    GROW_INPUT_ELEMENT.style.width = (INPUT_REFERENCE_CLONE.getBoundingClientRect()).width + 16 + 'px';
+
+    GROW_INPUT_ELEMENT.addEventListener('input', e => {
+        INPUT_REFERENCE_CLONE.innerHTML = e.target.value.replace(/\s/g, '&nbsp;');
+        e.target.style.width = (INPUT_REFERENCE_CLONE.getBoundingClientRect()).width + 16 + 'px';
+    });
+    
 });
