d22e69adbef985daca3beafcecf5045c65d32e4f
tomthestrom <thruska29@gmail.com>
2021-01-22 20:17:53 +0100

    removed redundant dependencies, installed kill-port to handle problematic process hanging after crash

--- app.js
+++ app.js
@@ -5,19 +5,28 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 const path = require("path");
-const logger = require("morgan");
 const sassMiddleware = require("node-sass-middleware");
 
 const indexRouter = require("./routes/index");
 const usersRouter = require("./routes/users");
 
 const app = express();
+app.use(cors());
+app.use(express.json());
+
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true });
+const connection = mongoose.connection;
+connection.once('open', () => {
+  console.log('Mongo DB connection established successfully')
+})
+
+
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
-app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(
