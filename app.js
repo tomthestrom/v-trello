143fb80fa9899eb343120633fbfb46a887d29115
tomthestrom <thruska29@gmail.com>
2021-01-24 18:12:03 +0100

    index.html -> index.ejs, app.js- nodesass middleware tinkering

--- app.js
+++ app.js
@@ -31,9 +31,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(
   sassMiddleware({
-    src: path.join(__dirname, "public"),
+    src: path.join(__dirname, "assets/stylesheets/*"),
     dest: path.join(__dirname, "public"),
-    indentedSyntax: true, // true = .sass and false = .scss
+    indentedSyntax: false, // true = .sass and false = .scss
     sourceMap: true,
   })
 );
