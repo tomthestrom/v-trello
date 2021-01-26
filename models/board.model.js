eb76cf98a80f2b9a76057b61c9e82071b2ede7ba
tomthestrom <thruska29@gmail.com>
2021-01-26 09:04:02 +0100

    board schema update

--- models/board.model.js
+++ models/board.model.js
@@ -2,12 +2,14 @@ const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
 
+
 const boardSchema = new Schema({
-    about: { type: String, required: false },
-    bg_color: { type: String, required: true },
-    bg_photo: { type: String, required: true },
-    board_id: { type: ObjectId, required: true },
-    title: { type: String, required: true }
+    about: { type: string, required: false },
+    bg_color: { type: string, required: true },
+    bg_photo: { type: string, required: true },
+    board_id: { type: objectId, required: true },
+    title: { type: string, required: true },
+    cards: {type: array, required: false}
 }, {
   timestamps: true,
 });
