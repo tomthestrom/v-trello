16d6979b3fa5d523b16cb9de06072f8cf9c7d98e
tomthestrom <thruska29@gmail.com>
2021-02-08 16:09:11 +0100

    list deck title growing TextArea done

--- assets/js/components/list/TextArea.js
+++ assets/js/components/list/TextArea.js
@@ -3,13 +3,28 @@ export default class TitleTextArea extends HTMLTextAreaElement {
         super();
     }
 
+    //base height of the element
+    get BASE_HEIGHT () {
+        return 33;
+    }
+    
     connectedCallback () {
         const eventHandlers = this.eventHandlers.call(this);
-        this.addEventListener('input', () => console.log('input textarea'))
+
+        this.addEventListener('input', eventHandlers.input);
     }
 
     eventHandlers () {
+        const input = () => {
+            //only grow it if it has not already been grown and there is anything to scroll (scrollheight>baseHeight) e
+            if (this.getBoundingClientRect().height !== this.scrollHeight) {
+                this.style.height = this.scrollHeight > this.BASE_HEIGHT ? (this.scrollHeight) + 'px' : this.BASE_HEIGHT;
+            }
+        }
 
+        return {
+            input
+        }
     }
 
 
