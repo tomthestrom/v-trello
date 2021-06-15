d0d2a7d3b9af04d13b62c0db07e6cc5783395ef6
tomthestrom <thruska29@gmail.com>
2021-06-15 15:20:01 +0200

    CardDeck component created, registered + added is="card-deck" to dummy elements

---
+++ assets/js/components/deck/CardDeck.js
@@ -0,0 +1,8 @@
+class CardDeck extends HTMLUListElement {
+
+    constructor () {
+        super();
+    }
+}
+
+export { CardDeck as CardDeckElement }
--- assets/js/index.js
+++ assets/js/index.js
@@ -4,10 +4,12 @@ import TitleInput from './components/board/Input';
 import TitleTextArea from './components/deck/TextArea';
 import AddList from './components/deck/AddList';
 import { CardListElement } from './components/deck/CardList';
-import { ListDeckElement } from './components/deck/List';
+import { ListDeckElement } from './components/deck/ListDeck';
 import { DropZoneElement } from './components/deck/DropZone';
 import socketConnection from './services/websocket';
 import { SingleCardElement } from './components/deck/SingleCard';
+import { CardDeckElement } from './components/deck/CardDeck';
+
 // @TODO: create a custom element register obj
 // // Connection opened
 socketConnection.addEventListener('open', function (event) {
@@ -36,3 +38,4 @@ customElements.define('card-list', CardListElement);
 customElements.define('list-deck', ListDeckElement, { extends: 'div'});
 customElements.define('drop-zone', DropZoneElement, { extends: 'div' });
 customElements.define('single-card', SingleCardElement, {extends: 'li'});
+customElements.define('card-deck', CardDeckElement, { extends: 'ul'});
